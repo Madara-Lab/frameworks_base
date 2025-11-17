f1a382e9700f09c2818f0a9d71085a0a5de0cecc
Pranav Vashi <neobuddy89@gmail.com>
2025-11-18 03:44:29 +0530

    MediaArt: Register tunable for all users
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    (cherry picked from commit 207e53a22aa3decbee667505d7ee7c175a5afbf7)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/MediaViewController.kt
+++ packages/SystemUI/src/com/android/systemui/media/MediaViewController.kt
@@ -109,27 +109,32 @@ class MediaViewController @Inject constructor(
         context.contentResolver.registerContentObserver(
             Settings.System.getUriFor(Settings.System.LS_MEDIA_ART_ENABLED),
             false,
-            settingsObserver
+            settingsObserver,
+            UserHandle.USER_ALL
         )
         context.contentResolver.registerContentObserver(
             Settings.System.getUriFor(Settings.System.LS_MEDIA_ART_AOD_ENABLED),
             false,
-            settingsObserver
+            settingsObserver,
+            UserHandle.USER_ALL
         )
         context.contentResolver.registerContentObserver(
             Settings.System.getUriFor(Settings.System.LS_MEDIA_ART_FILTER),
             false,
-            settingsObserver
+            settingsObserver,
+            UserHandle.USER_ALL
         )
         context.contentResolver.registerContentObserver(
             Settings.System.getUriFor(Settings.System.LS_MEDIA_ART_FADE_LEVEL),
             false,
-            settingsObserver
+            settingsObserver,
+            UserHandle.USER_ALL
         )
         context.contentResolver.registerContentObserver(
             Settings.System.getUriFor(Settings.System.LS_MEDIA_ART_BLUR_LEVEL),
             false,
-            settingsObserver
+            settingsObserver,
+            UserHandle.USER_ALL
         )
         context.contentResolver.registerContentObserver(
             Settings.System.getUriFor(Settings.System.LS_MEDIA_ART_PIXEL_SIZE),
