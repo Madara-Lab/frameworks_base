be63da4a09ab5f80eb0013c519d12df6249cd19a
Ghosuto <clash.raja10@gmail.com>
2026-03-07 08:05:09 +0000

    SystemUI: Add volume percentage [1/2]
    - Only for redesigend style
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7651,6 +7651,12 @@ public final class Settings {
         public static final String SHOW_APP_VOLUME = "show_app_volume";
 
         /**
+         * Whether to show volume percentage in volume panel
+         * @hide
+         */
+        public static final String SHOW_VOLUME_PERCENTAGE = "show_volume_percentage";
+
+        /**
          * Force full screen for devices with cutout
          * @hide
          */
