789b86c0591831747d9fa3ae3579eeb74d1b20e4
beingashwani <ashwanic177@gmail.com>
2026-07-12 18:49:47 +0000

    SystemUI: Add one-time hotspot data limit (1/2)
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    (cherry picked from commit 9bd09e86b7070e91b387f13e45aaca8ef8398dbd)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -39,6 +39,7 @@ import com.android.systemui.dreams.homecontrols.system.HomeControlsDreamStartabl
 import com.android.systemui.globalactions.GlobalActionsComponent
 import com.android.systemui.haptics.msdl.MSDLCoreStartable
 import com.android.systemui.keyboard.KeyboardUI
+import com.android.systemui.hotspot.HotspotDataLimitController
 import com.android.systemui.keyboard.PhysicalKeyboardCoreStartable
 import com.android.systemui.keyevent.SysUIKeyGestureEventInitializer
 import com.android.systemui.keyguard.KeyguardViewConfigurator
@@ -362,4 +363,10 @@ abstract class SystemUICoreStartableModule {
     abstract fun bindSmartPixelManager(
         impl: SmartPixelManager
     ): CoreStartable
+
+    /** Inject into HotspotDataLimitController. */
+    @Binds
+    @IntoMap
+    @ClassKey(HotspotDataLimitController::class)
+    abstract fun bindHotspotDataLimitController(impl: HotspotDataLimitController): CoreStartable
 }
