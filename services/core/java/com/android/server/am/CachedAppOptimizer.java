3f1b78c38d8ee680ee61117745651c1c1ab896fe
Ghosuto <clash.raja10@gmail.com>
2026-07-18 05:59:09 +0000

    SystemUI: DynamicBar: Exclude timer, stopwatch and recording from circle style
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    (cherry picked from commit 7c65825b35229150ce5597ba536ef9add1507cb9)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -334,7 +334,8 @@ public class CachedAppOptimizer {
         NONE, // No compaction
         SOME, // File compaction
         ANON, // Anon compaction
-        FULL // File+anon compaction
+        FULL, // File+anon compaction
+        POPULATE // Populate/prefetch compaction
     }
 
     // This indicates who initiated the compaction request
